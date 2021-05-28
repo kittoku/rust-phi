b56bdbbc97e9f03ca92907d21342eefe16c30ebf
KOBAYASHI Ittoku <nono381d815@gmail.com>
2021-05-28 21:34:06 +0900

    CLN: fix style

--- src/emd.rs
+++ src/emd.rs
@@ -2,10 +2,10 @@ use nalgebra as na;
 use minilp::{LinearExpr, Problem, Solution};
 
 
-fn check_dimension(P: &na::DVector<f64>, Q: &na::DVector<f64>) -> usize {
-    let ndim = P.len();
+fn check_dimension(vec_from: &na::DVector<f64>, vec_to: &na::DVector<f64>) -> usize {
+    let ndim = vec_from.len();
     assert!(ndim > 0);
-    assert!(Q.len() == ndim);
+    assert!(vec_to.len() == ndim);
 
     ndim
 }
@@ -16,8 +16,8 @@ fn generate_empty_exprs(capacity: usize) -> Vec<LinearExpr> {
     vec
 }
 
-pub fn calc_emd_for_mechanism(P: &na::DVector<f64>, Q: &na::DVector<f64>) -> Solution {
-    let ndim = check_dimension(P, Q);
+pub fn calc_emd_for_mechanism(vec_from: &na::DVector<f64>, vec_to: &na::DVector<f64>) -> Solution {
+    let ndim = check_dimension(vec_from, vec_to);
     let mut problem = Problem::new(minilp::OptimizationDirection::Minimize);
     let mut sum_rows = generate_empty_exprs(ndim);
     let mut sum_columns = generate_empty_exprs(ndim);
@@ -32,8 +32,8 @@ pub fn calc_emd_for_mechanism(P: &na::DVector<f64>, Q: &na::DVector<f64>) -> Sol
         });
     });
 
-    P.iter().zip(sum_rows).for_each(|(&p, expr)| problem.add_constraint(expr, minilp::ComparisonOp::Eq, p));
-    Q.iter().zip(sum_columns).for_each(|(&q, expr)| problem.add_constraint(expr, minilp::ComparisonOp::Eq, q));
+    vec_from.iter().zip(sum_rows).for_each(|(&p, expr)| problem.add_constraint(expr, minilp::ComparisonOp::Eq, p));
+    vec_to.iter().zip(sum_columns).for_each(|(&q, expr)| problem.add_constraint(expr, minilp::ComparisonOp::Eq, q));
 
     problem.solve().unwrap()
 }
