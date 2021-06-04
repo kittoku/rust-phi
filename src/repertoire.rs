d7fe4d940fa31dbb8b527b237efaba3efd9e6dd6
KOBAYASHI Ittoku <nono381d815@gmail.com>
2021-06-04 12:31:37 +0900

    DOC: use terms according to paper

--- src/repertoire.rs
+++ src/repertoire.rs
@@ -12,34 +12,34 @@ pub fn normalize_repertoire(repertoire: &mut na::DVector<f64>, sum: Option<f64>)
     repertoire.apply(|x| x * norm_term);
 }
 
-fn calc_unconstrained_cause(past_bases: &BitBases, tpm: &na::DMatrix<f64>) -> na::DVector<f64> {
+fn calc_unconstrained_cause(purview: &BitBases, tpm: &na::DMatrix<f64>) -> na::DVector<f64> {
     let ndim = tpm.nrows();
-    let p = 1.0 / past_bases.image_size() as f64;
+    let p = 1.0 / purview.image_size() as f64;
     na::DVector::<f64>::from_element(ndim, p)
 }
 
-fn calc_elementary_cause(past_bases: &BitBases, current_bases: &BitBases, current_state: usize, tpm: &na::DMatrix<f64>) -> na::DVector<f64> {
+fn calc_elementary_cause(purview: &BitBases, mechanism: &BitBases, current_state: usize, tpm: &na::DMatrix<f64>) -> na::DVector<f64> {
     let ndim = tpm.nrows();
 
-    let c_current_bases = current_bases.generate_complement_bases();
+    let c_mechanism = mechanism.generate_complement_bases();
 
     let mut accumulated = na::DVector::<f64>::zeros(ndim);
-    let fixed_current = current_bases.bases[0] & current_state;
-    c_current_bases.span(fixed_current).for_each(|col| {
+    let fixed_current = mechanism.bases[0] & current_state;
+    c_mechanism.span(fixed_current).for_each(|col| {
         accumulated += tpm.column(col);
     });
 
 
-    let c_past_bases = past_bases.generate_complement_bases();
+    let c_purview = purview.generate_complement_bases();
 
     let mut marginal = na::DVector::<f64>::zeros(ndim);
-    past_bases.span(0).for_each(|eq_class| {
-        let mut marginal_rows = Vec::<usize>::with_capacity(c_past_bases.image_size());
+    purview.span(0).for_each(|eq_class| {
+        let mut marginal_rows = Vec::<usize>::with_capacity(c_purview.image_size());
 
-        if c_past_bases.dim == 0 {
+        if c_purview.dim == 0 {
             marginal_rows.push(eq_class);
         } else {
-            c_past_bases.span(eq_class).for_each(|row| {
+            c_purview.span(eq_class).for_each(|row| {
                 marginal_rows.push(row);
             });
         }
@@ -53,46 +53,46 @@ fn calc_elementary_cause(past_bases: &BitBases, current_bases: &BitBases, curren
         });
     });
 
-    normalize_repertoire(&mut marginal, Some(c_past_bases.image_size() as f64));
+    normalize_repertoire(&mut marginal, Some(c_purview.image_size() as f64));
     marginal
 }
 
-pub fn calc_cause_repertoire(past_bases: &BitBases, current_bases: &BitBases, current_state: usize, tpm: &na::DMatrix<f64>) -> na::DVector<f64> {
-    if past_bases.dim == 0 {
+pub fn calc_cause_repertoire(purview: &BitBases, mechanism: &BitBases, current_state: usize, tpm: &na::DMatrix<f64>) -> na::DVector<f64> {
+    if purview.dim == 0 {
         return na::DVector::<f64>::from_element(tpm.nrows(), 1.0);
     }
 
-    match current_bases.dim {
-        0 => calc_unconstrained_cause(past_bases, tpm),
-        1 => calc_elementary_cause(past_bases, current_bases, current_state, tpm),
+    match mechanism.dim {
+        0 => calc_unconstrained_cause(purview, tpm),
+        1 => calc_elementary_cause(purview, mechanism, current_state, tpm),
         _ => {
-            let mut joint = calc_elementary_cause(past_bases, &current_bases.sub_bases(&[0]), current_state, tpm);
+            let mut joint = calc_elementary_cause(purview, &mechanism.sub_bases(&[0]), current_state, tpm);
 
-            (1..current_bases.dim).for_each(|i| {
-                joint.component_mul_assign(&calc_elementary_cause(past_bases, &current_bases.sub_bases(&[i]), current_state, tpm));
+            (1..mechanism.dim).for_each(|i| {
+                joint.component_mul_assign(&calc_elementary_cause(purview, &mechanism.sub_bases(&[i]), current_state, tpm));
             });
 
-            normalize_repertoire(&mut joint, Some(past_bases.codim_image_size() as f64));
+            normalize_repertoire(&mut joint, Some(purview.codim_image_size() as f64));
             joint
         }
     }
 }
 
-fn calc_elementary_effect(future_bases: &BitBases, current_bases: &BitBases, current_state: usize, tpm: &na::DMatrix<f64>) -> na::DVector<f64> {
+fn calc_elementary_effect(purview: &BitBases, mechanism: &BitBases, current_state: usize, tpm: &na::DMatrix<f64>) -> na::DVector<f64> {
     let ndim = tpm.nrows();
 
     let mut accumulated = na::DVector::<f64>::zeros(ndim).transpose();
-    let c_current_bases = current_bases.generate_complement_bases();
-    c_current_bases.span(current_bases.fixed_state(current_state)).for_each(|row| {
+    let c_mechanism = mechanism.generate_complement_bases();
+    c_mechanism.span(mechanism.fixed_state(current_state)).for_each(|row| {
         accumulated += tpm.row(row);
     });
 
 
     let mut marginal = na::DVector::<f64>::zeros(ndim);
-    let c_future_bases = future_bases.generate_complement_bases();
-    future_bases.span(0).for_each(|eq_class| {
-        let mut cols = Vec::<usize>::with_capacity(c_future_bases.image_size());
-        c_future_bases.span(eq_class).for_each(|col| {
+    let c_purview = purview.generate_complement_bases();
+    purview.span(0).for_each(|eq_class| {
+        let mut cols = Vec::<usize>::with_capacity(c_purview.image_size());
+        c_purview.span(eq_class).for_each(|col| {
             cols.push(col);
         });
 
@@ -103,25 +103,25 @@ fn calc_elementary_effect(future_bases: &BitBases, current_bases: &BitBases, cur
         });
     });
 
-    normalize_repertoire(&mut marginal, Some(c_future_bases.image_size() as f64));
+    normalize_repertoire(&mut marginal, Some(c_purview.image_size() as f64));
     marginal
 }
 
-pub fn calc_effect_repertoire(future_bases: &BitBases, current_bases: &BitBases, current_state: usize, tpm: &na::DMatrix<f64>) -> na::DVector<f64> {
-    if future_bases.dim == 0 {
+pub fn calc_effect_repertoire(purview: &BitBases, mechanism: &BitBases, current_state: usize, tpm: &na::DMatrix<f64>) -> na::DVector<f64> {
+    if purview.dim == 0 {
         return na::DVector::<f64>::from_element(tpm.nrows(), 1.0);
     }
 
-    if future_bases.dim == 1 {
-        calc_elementary_effect(future_bases, current_bases, current_state, tpm)
+    if purview.dim == 1 {
+        calc_elementary_effect(purview, mechanism, current_state, tpm)
     } else {
-        let mut joint = calc_elementary_effect(&future_bases.sub_bases(&[0]), current_bases, current_state, tpm);
+        let mut joint = calc_elementary_effect(&purview.sub_bases(&[0]), mechanism, current_state, tpm);
 
-        (1..future_bases.dim).for_each(|i| {
-            joint.component_mul_assign(&calc_elementary_effect(&future_bases.sub_bases(&[i]), current_bases, current_state, tpm));
+        (1..purview.dim).for_each(|i| {
+            joint.component_mul_assign(&calc_elementary_effect(&purview.sub_bases(&[i]), mechanism, current_state, tpm));
         });
 
-        normalize_repertoire(&mut joint, Some(future_bases.codim_image_size() as f64));
+        normalize_repertoire(&mut joint, Some(purview.codim_image_size() as f64));
         joint
     }
 }
--- src/tests.rs
+++ src/tests.rs
@@ -94,76 +94,76 @@ fn test_calc_cause_repertoire() {
     let tpm = generate_reference_tpm();
     let current_state = generate_reference_state();
 
-    let mut past_masks = Vec::<usize>::new();
-    let mut current_masks = Vec::<usize>::new();
+    let mut purview_masks = Vec::<usize>::new();
+    let mut mechanism_masks = Vec::<usize>::new();
     let mut expected = Vec::<na::DVector<f64>>::new();
 
     // CASE 0, Fig.4 p(ABC^p|A^c=1)
-    past_masks.push(0b111);
-    current_masks.push(0b001);
+    purview_masks.push(0b111);
+    mechanism_masks.push(0b001);
     let mut e_0 = na::DVector::<f64>::from_column_slice(&[0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]);
     normalize_repertoire(&mut e_0, None);
     expected.push(e_0);
 
     // CASE 1, Fig.10 p(AB^p|C^c), ,
-    past_masks.push(0b011);
-    current_masks.push(0b100);
+    purview_masks.push(0b011);
+    mechanism_masks.push(0b100);
     let mut e_1 = na::DVector::<f64>::from_column_slice(&[1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0]);
     normalize_repertoire(&mut e_1, None);
     expected.push(e_1);
 
     // CASE 2, Fig.10 p(AC^p|B^c)
-    past_masks.push(0b101);
-    current_masks.push(0b010);
+    purview_masks.push(0b101);
+    mechanism_masks.push(0b010);
     let mut e_2 = na::DVector::<f64>::from_column_slice(&[1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 1.0, 0.0]);
     normalize_repertoire(&mut e_2, None);
     expected.push(e_2);
 
     // CASE 3, Fig.10 p(BC^p|A^c)
-    past_masks.push(0b110);
-    current_masks.push(0b001);
+    purview_masks.push(0b110);
+    mechanism_masks.push(0b001);
     let mut e_3 = na::DVector::<f64>::from_column_slice(&[0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]);
     normalize_repertoire(&mut e_3, None);
     expected.push(e_3);
 
     // CASE 4, Fig.10 p(ABC^p|ABC^c)
-    past_masks.push(0b111);
-    current_masks.push(0b111);
+    purview_masks.push(0b111);
+    mechanism_masks.push(0b111);
     let mut e_4 = na::DVector::<f64>::from_column_slice(&[0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0]);
     normalize_repertoire(&mut e_4, None);
     expected.push(e_4);
 
     // CASE 5, Fig.10 p(AB^p|BC^c)
-    past_masks.push(0b011);
-    current_masks.push(0b110);
+    purview_masks.push(0b011);
+    mechanism_masks.push(0b110);
     let mut e_5 = na::DVector::<f64>::from_column_slice(&[2.0, 0.0, 0.0, 1.0, 2.0, 0.0, 0.0, 1.0]);
     normalize_repertoire(&mut e_5, None);
     expected.push(e_5);
 
     // CASE 6, Fig.10 p(ABC^p|AB^c)
-    past_masks.push(0b111);
-    current_masks.push(0b011);
+    purview_masks.push(0b111);
+    mechanism_masks.push(0b011);
     let mut e_6 = na::DVector::<f64>::from_column_slice(&[0.0, 0.0, 1.0, 1.0, 1.0, 0.0, 1.0, 0.0]);
     normalize_repertoire(&mut e_6, None);
     expected.push(e_6);
 
     // CASE 7, p(ABC^p)
-    past_masks.push(0b111);
-    current_masks.push(0b000);
+    purview_masks.push(0b111);
+    mechanism_masks.push(0b000);
     let mut e_7 = na::DVector::<f64>::from_column_slice(&[1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]);
     normalize_repertoire(&mut e_7, None);
     expected.push(e_7);
 
     // CASE 8, p([])
-    past_masks.push(0b000);
-    current_masks.push(0b000);
+    purview_masks.push(0b000);
+    mechanism_masks.push(0b000);
     let mut e_8 = na::DVector::<f64>::from_column_slice(&[1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]);
     normalize_repertoire(&mut e_8, None);
     expected.push(e_8);
 
     // CASE 9, p([]|ABC^c)
-    past_masks.push(0b000);
-    current_masks.push(0b111);
+    purview_masks.push(0b000);
+    mechanism_masks.push(0b111);
     let mut e_9 = na::DVector::<f64>::from_column_slice(&[1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]);
     normalize_repertoire(&mut e_9, None);
     expected.push(e_9);
@@ -171,12 +171,12 @@ fn test_calc_cause_repertoire() {
 
     let max_dim = (tpm.nrows() - 1).count_ones() as usize;
     expected.iter().enumerate().for_each(|(i, e)| {
-        let past_bases = BitBases::construct_from_mask(past_masks[i], max_dim);
-        let c_past_bases = past_bases.generate_complement_bases();
-        let current_bases =BitBases::construct_from_mask(current_masks[i], max_dim);
+        let purview = BitBases::construct_from_mask(purview_masks[i], max_dim);
+        let c_purview = purview.generate_complement_bases();
+        let mechanism =BitBases::construct_from_mask(mechanism_masks[i], max_dim);
 
-        let marginal = calc_cause_repertoire(&past_bases, &current_bases, current_state, &tpm);
-        let unconstrained = calc_cause_repertoire(&c_past_bases, &BitBases::null_bases(max_dim), current_state, &tpm);
+        let marginal = calc_cause_repertoire(&purview, &mechanism, current_state, &tpm);
+        let unconstrained = calc_cause_repertoire(&c_purview, &BitBases::null_bases(max_dim), current_state, &tpm);
 
         let actual  = marginal.component_mul(&unconstrained);
 
@@ -190,13 +190,13 @@ fn test_calc_effect_repertoire() {
     let tpm = generate_reference_tpm();
     let current_state = generate_reference_state();
 
-    let mut future_masks = Vec::<usize>::new();
-    let mut current_masks = Vec::<usize>::new();
+    let mut purview_masks = Vec::<usize>::new();
+    let mut mechanism_masks = Vec::<usize>::new();
     let mut expected = Vec::<na::DVector<f64>>::new();
 
     // CASE 0, p(ABC^f)
-    future_masks.push(0b111);
-    current_masks.push(0b000);
+    purview_masks.push(0b111);
+    mechanism_masks.push(0b000);
     let (a_0, a_1) = (1.0 / 4.0, 3.0 / 4.0);
     let uc_a = na::DVector::<f64>::from_column_slice(&[a_0, a_1, a_0, a_1, a_0, a_1, a_0, a_1]);
     let (b_0, b_1) = (3.0 / 4.0, 1.0 / 4.0);
@@ -206,48 +206,48 @@ fn test_calc_effect_repertoire() {
     expected.push(uc_abc.clone());
 
     // CASE 1, p([])
-    future_masks.push(0b000);
-    current_masks.push(0b000);
+    purview_masks.push(0b000);
+    mechanism_masks.push(0b000);
     expected.push(uc_abc.clone());
 
     // CASE 2, Fig.10 p(AC^f|ABC^c)
-    future_masks.push(0b101);
-    current_masks.push(0b111);
+    purview_masks.push(0b101);
+    mechanism_masks.push(0b111);
     let mut m_2 = na::DVector::<f64>::from_column_slice(&[0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0]);
     normalize_repertoire(&mut m_2, Some(2.0));
     expected.push(m_2.component_mul(&uc_b));
 
     // CASE 3, Fig.10 p(A^f|BC^c)
-    future_masks.push(0b001);
-    current_masks.push(0b110);
+    purview_masks.push(0b001);
+    mechanism_masks.push(0b110);
     let mut m_3 = na::DVector::<f64>::from_column_slice(&[2.0, 0.0, 2.0, 0.0, 2.0, 0.0, 2.0, 0.0]);
     normalize_repertoire(&mut m_3, Some(4.0));
     expected.push(m_3.component_mul(&uc_b).component_mul(&uc_c));
 
     // CASE 4, Fig.10 p(C^f|AB^c)
-    future_masks.push(0b100);
-    current_masks.push(0b011);
+    purview_masks.push(0b100);
+    mechanism_masks.push(0b011);
     let mut m_4 = na::DVector::<f64>::from_column_slice(&[0.0, 0.0, 0.0, 0.0, 2.0, 2.0, 2.0, 2.0]);
     normalize_repertoire(&mut m_4, Some(4.0));
     expected.push(m_4.component_mul(&uc_a).component_mul(&uc_b));
 
     // CASE 5, Fig.10 p(AB^f|C^c)
-    future_masks.push(0b011);
-    current_masks.push(0b100);
+    purview_masks.push(0b011);
+    mechanism_masks.push(0b100);
     let mut m_5 = na::DVector::<f64>::from_column_slice(&[2.0, 2.0, 0.0, 0.0, 2.0, 2.0, 0.0, 0.0]);
     normalize_repertoire(&mut m_5, Some(2.0));
     expected.push(m_5.component_mul(&uc_c));
 
     // CASE 6, Fig.10 p(A^f|B^c)
-    future_masks.push(0b001);
-    current_masks.push(0b100);
+    purview_masks.push(0b001);
+    mechanism_masks.push(0b100);
     let mut m_6 = na::DVector::<f64>::from_column_slice(&[2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0]);
     normalize_repertoire(&mut m_6, Some(4.0));
     expected.push(m_6.component_mul(&uc_b).component_mul(&uc_c));
 
     // CASE 7, Fig.10 p(B^f|A^c)
-    future_masks.push(0b010);
-    current_masks.push(0b001);
+    purview_masks.push(0b010);
+    mechanism_masks.push(0b001);
     let mut m_7 = na::DVector::<f64>::from_column_slice(&[2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0]);
     normalize_repertoire(&mut m_7, Some(4.0));
     expected.push(m_7.component_mul(&uc_a).component_mul(&uc_c));
@@ -255,12 +255,12 @@ fn test_calc_effect_repertoire() {
 
     let max_dim = (tpm.nrows() - 1).count_ones() as usize;
     expected.iter().enumerate().for_each(|(i, e)| {
-        let future_bases = BitBases::construct_from_mask(future_masks[i], max_dim);
-        let c_future_bases = future_bases.generate_complement_bases();
-        let current_bases =BitBases::construct_from_mask(current_masks[i], max_dim);
+        let purview = BitBases::construct_from_mask(purview_masks[i], max_dim);
+        let c_purview = purview.generate_complement_bases();
+        let mechanism =BitBases::construct_from_mask(mechanism_masks[i], max_dim);
 
-        let marginal = calc_effect_repertoire(&future_bases, &current_bases, current_state, &tpm);
-        let unconstrained = calc_effect_repertoire(&c_future_bases, &BitBases::null_bases(max_dim), current_state, &tpm);
+        let marginal = calc_effect_repertoire(&purview, &mechanism, current_state, &tpm);
+        let unconstrained = calc_effect_repertoire(&c_purview, &BitBases::null_bases(max_dim), current_state, &tpm);
 
         let actual  = marginal.component_mul(&unconstrained);
 
